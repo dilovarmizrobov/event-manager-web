cd5785430a102bb99319ef7db1aa056c769ef67e
Surush <surush.t@abad.tj>
2022-05-09 15:33:33 +0500

    Implemented event locations CRUD

--- src/services/EventLocationService.ts
+++ src/services/EventLocationService.ts
@@ -1,8 +1,27 @@
 import api from "../utils/api";
+import {ILocation} from "../models/ILocation";
 
 class EventLocationService {
+    getLocation = (locationId: string) => new Promise((resolve, reject)  => {
+        api.get(`/locations/${locationId}`)
+            .then(response => resolve(response.data))
+            .catch(error => reject(error))
+    })
+
     getLocations = () => new Promise((resolve, reject)  => {
         api.get(`/locations`)
+            .then(response => resolve(response.data))
+            .catch(error => reject(error))
+    })
+
+    postNewLocation = (location: ILocation) => new Promise((resolve, reject) => {
+        api.post(`/locations`, location)
+            .then(response => resolve(response.data))
+            .catch(error => reject(error))
+    })
+
+    putUpdateLocation = (location: ILocation) => new Promise((resolve, reject) => {
+        api.put(`/locations/${location.id!}`, location)
             .then(response => resolve(response.data))
             .catch(error => reject(error))
     })
---
+++ src/views/event-location/list/EventLocationListView.tsx
@@ -0,0 +1,115 @@
+import React, {useEffect, useState} from 'react';
+import Page from "../../../components/Page";
+import {styled} from "@mui/material/styles";
+import {
+    Box,
+    Card,
+    Container,
+    IconButton,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@mui/material";
+import Header from "./Header";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import {useSnackbar} from "notistack";
+import {ILocation} from "../../../models/ILocation";
+import errorMessageHandler from "../../../utils/errorMessageHandler";
+import eventLocationService from "../../../services/EventLocationService";
+import NoFoundTableBody from "../../../components/NoFoundTableBody";
+import {FiEdit, FiTrash} from "react-icons/fi";
+import {NavLink as RouterLink} from "react-router-dom";
+
+const Root = styled('div')(({theme}) => ({
+    minHeight: '100%',
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3)
+}))
+
+const EventLocationListView = () => {
+    const {enqueueSnackbar} = useSnackbar()
+    const [loading, setLoading] = useState(false)
+    const [rows, setRows] = useState<ILocation[]>([])
+
+    useEffect(() => {
+        let cancel = false;
+
+        (async () => {
+            try {
+                setLoading(true)
+                setRows([])
+
+                const data: any = await eventLocationService.getLocations()
+
+                if (!cancel) setRows(data)
+            } catch (error: any) {
+                enqueueSnackbar(errorMessageHandler(error), {variant: 'error'})
+            } finally {
+                !cancel && setLoading(false)
+            }
+        })()
+
+        return () => {cancel = true}
+    },[])
+
+    return (
+        <>
+            <Page title="Места проведения"/>
+            <Root>
+                <Container maxWidth="xl">
+                    <Header/>
+                    <Box mt={3}>
+                        <Card>
+                            <PerfectScrollbar>
+                                <Box minWidth={750} sx={{mb: 2}}>
+                                    <TableContainer>
+                                        <Table>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell>№</TableCell>
+                                                    <TableCell>Место</TableCell>
+                                                    <TableCell/>
+                                                </TableRow>
+                                            </TableHead>
+                                            {
+                                                rows.length > 0 ? (
+                                                    <TableBody>
+                                                        {
+                                                            rows.map((row, index) => (
+                                                                <TableRow hover key={row.id}>
+                                                                    <TableCell>{index + 1}</TableCell>
+                                                                    <TableCell>{row.name}</TableCell>
+                                                                    <TableCell style={{ width: 165 }}>
+                                                                        <IconButton
+                                                                            size="large"
+                                                                            component={RouterLink}
+                                                                            to={`/event-locations/${row.id}/edit`}
+                                                                        >
+                                                                            <FiEdit size={20} />
+                                                                        </IconButton>
+                                                                        <IconButton size="large">
+                                                                            <FiTrash size={20} />
+                                                                        </IconButton>
+                                                                    </TableCell>
+                                                                </TableRow>
+                                                            ))
+                                                        }
+                                                    </TableBody>
+                                                ) : <NoFoundTableBody loading={loading}/>
+                                            }
+                                        </Table>
+                                    </TableContainer>
+                                </Box>
+                            </PerfectScrollbar>
+                        </Card>
+                    </Box>
+                </Container>
+            </Root>
+        </>
+    );
+};
+
+export default EventLocationListView;
